55d7f90a3c318288c362d43d4cebc984711dabbd
Oleh <opolonetso@gmail.com>
2022-07-02 14:03:48 +0300

    add footer and modal window

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
 $(document).ready(function () {
     let currentFloor = 2;
+    let currentFlat = 30;
+    let currentLink = 40;
     let counterUp = $(".counter-up");
     let counterDown = $(".counter-down");
     let floorPath = $(".home-image path");
+    let modal = $(".modal");
+    let modalCloseButton = $(".modal-close-button");
+    let viewFlatsButton = $(".view-flats");
+    let flatsPath = $(".flats path");
+    let flatLink = $(".flat-list a");
+
     floorPath.on("mouseover", function () {
         floorPath.removeClass("current-floor")
         currentFloor = $(this).attr("data-floor");
         $(".counter").text(currentFloor);
     });
+
+    /*
+
+    flatsPath.on("mouseover", function () {
+        flatLink.removeClass("active");
+        currentFlat = $(this).attr("data-flat");
+        console.log(currentFlat);
+    });
+
+    if (currentFlat == currentLink) {
+        $(".number-flat").text(currentFlat);
+        //  flatLink.toggleClass("active");
+        console.log("12345");
+    } else {
+        console.log('qwda');
+    }
+
+    flatLink.on("mouseover", function () {
+        flatLink.removeClass("active");
+        currentLink = $(this).attr("data-link");
+        console.log(currentLink);
+        flatLink.addClass("active");
+    });
+
+    */
+
+    floorPath.on("click", toggleModal);
+
+    modalCloseButton.on("click", toggleModal);
+
+    viewFlatsButton.on("click", toggleModal);
 
     counterUp.on("click", function () {
         if (currentFloor < 18) {
@@ -16,7 +55,7 @@ $(document).ready(function () {
                 minimumIntegerDigits: 2, useGrouping: false
             });
             $(".counter").text(usCurrentFloor);
-            floorPath.removeClass("current-floor")
+            floorPath.removeClass("current-floor");
             $(`[data-floor= ${usCurrentFloor}]`).toggleClass("current-floor");
         }
     })
@@ -32,4 +71,8 @@ $(document).ready(function () {
             $(`[data-floor= ${usCurrentFloor}]`).toggleClass("current-floor");
         }
     })
+
+    function toggleModal() {
+        modal.toggleClass("is-open");
+    }
 });
